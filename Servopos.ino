e5d40d83179916a5db8541147ad64fe755620b7e
Guilherme Aquino <31013254+itsmyvibee@users.noreply.github.com>
2018-08-17 15:59:22 -0300

    servo ino program

---
+++ Servopos.ino
@@ -0,0 +1,24 @@
+  //int delay =1000; //delay;
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+
+void setup() {
+  Serial.begin(9600); //starta porta serial
+  myservo.attach(10);  // attaches the servo on pin 9 to the servo object
+}
+
+void loop() {
+  while(Serial.available()==0){}; //espera por info pela porta serial
+  int pos = Serial.read(); // le a porta serial
+  Serial.print(pos);
+  
+  //delay(2000);
+  
+  myservo.write(pos);                  // sets the servo position according to the scaled value
+  
+  
+}
+
+
+
